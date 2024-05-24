fb7d6aa904203ed9bd6a7a97cfd44ba506f4ba8f
GENERHONE <papaarounafofana@gmail.com>
2024-05-24 20:24:49 +0000

    first commit

---
+++ Models/Pizza.cs
@@ -0,0 +1,12 @@
+namespace PAFPizza.Models; 
+
+public class Pizza { 
+    
+    public int Id { get; set; } 
+    
+    public string? Nom { get; set; } 
+    
+    public bool SansSucre { get; set; } 
+
+
+}
---
+++ PizzaServices/PizzaService.cs
@@ -0,0 +1,31 @@
+using PAFPizza.Models;
+namespace PAFPizza.Services; 
+public static class PizzaService { 
+	static List<Pizza> Pizzas { get; } 
+	static int nextId = 3; 
+	static PizzaService() 
+	{ 
+		Pizzas = new List<Pizza> { 
+			     new Pizza { Id = 1, Nom = "Classique Italien", SansSucre = false }, 
+			     new Pizza { Id = 2, Nom = "Veggie", SansSucre = true }
+		}; 
+	} 
+	public static List<Pizza> GetAll() => Pizzas; 
+	public static Pizza? Get(int id) => Pizzas.FirstOrDefault(p => p.Id == id); 
+	public static void Add(Pizza pizza) { 
+		pizza.Id = nextId++; 
+		Pizzas.Add(pizza);
+	} 
+	public static void Delete(int id) { 
+		var pizza = Get(id); 
+		if (pizza is null) 
+			return; 
+		Pizzas.Remove(pizza); 
+	} 
+	public static void Update(Pizza pizza) { 
+		var index = Pizzas.FindIndex(p => p.Id == pizza.Id); 
+		if (index == -1) 
+			  return; 
+		  Pizzas[index] = pizza; 
+	}
+}
